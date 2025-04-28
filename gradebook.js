95aab63b414e85d03ca10ebd54f50a3db4c06c42
impena <impena@my.waketech.edu>
2025-04-27 20:10:34 -0400

    Add files via upload

---
+++ gradebook.js
@@ -0,0 +1,14 @@
+// TODO: fetch data from the PostgreSQL database (implemented later)
+function fetchGradeData() {
+	console.log("Fetching grade data...");
+}
+
+// TODO: populate the table with grade data
+function populateGradebook(data) {
+	console.log("Populating gradebook with data:", data);
+}
+
+// TODO REMOVE THIS
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
+// END REMOVE
